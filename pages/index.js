dfb1d872d08f398fc0d13c1058af16432ef9fad8
Amulya Baral <amulyabaral2@gmail.com>
2025-05-10 13:24:51 +0200

    minor

--- pages/index.js
+++ pages/index.js
@@ -356,7 +356,7 @@ IMPORTANT:
                     <span className={styles.logoText}>nuggs.ai</span>
                 </div>
                 <p className={styles.tagline}>
-                    The future is now. AI will help you make the best nuggets of your life, and more..
+                    This AI won't steal your job. It will instead help you make the best nuggets of your life, and more..
                 </p>
                 <nav className={styles.toolPillNavigation}>
                     {tools.map(tool => (
