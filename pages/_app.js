e2c70d93745baf737c83fac00d59628a501c7386
Amulya Baral <amulyabaral2@gmail.com>
2025-05-10 18:25:08 +0200

    added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +9,22 @@ function MyApp({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-NWRYJEY849"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NWRYJEY849');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
